7bd0236df1bcfe2a3837fda1b29bf7e54d5925b1
brianAKB <brian.brown@snowsoftware.com>
2022-08-31 16:58:50 -0400

    generated nav bar

--- ReactJS-Practice/src/components/Navbar.jsx
+++ ReactJS-Practice/src/components/Navbar.jsx
@@ -1,5 +1,12 @@
+import logo from '../assets/react-icon-small.png'
+import '../style/style.css'
+
 export default function Navbar() {
     return (
-        <h1>Navbar Component</h1>
+        <nav>
+            <img src={logo} className="nav--icon"/>
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
     )
 }
